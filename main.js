ac2fb9ea8c64204b6aa1df21576df4902b4a0a48
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-01-23 11:14:49 -0700

    fixed picture in api

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 $("#call").on("click", function(){
   var pokemon = $("#title").val();
+  // var pokemon2 = $("#title").val();
 
   $.ajax({
     url: 'http://pokeapi.co/api/v1/pokemon/'+pokemon+'/',
     method: 'GET',
   }).done(function(response) {
     console.log(response);
+    $.ajax({
+      url: 'http://pokeapi.co/api/v1/sprite/'+(+pokemon+1)+'/',
+      method: 'GET',
+    }).done(function(response2) {
+      console.log(response2)
     $("#results").html('  ');
       $("#results").append("<h2>"+response.name+"</h2>");
+      //loop
       $("#results").append("<h3>Type: "+response.types[0].name+"</h3>");
-      $("#results").append("<h4>Abilities: "+response.abilities[0].name+", "+response.abilities[1].name+"</h4>");
+      //loop
+      // $("#results").append("<h4>Abilities: "+response.abilities[0].name+", "+response.abilities[1].name+"</h4>");
+      $("#results").append('<img src=http://pokeapi.co'+response2.image+'/>');
       $("#title").val('');
-  }); $.ajax({
-    url: 'http://pokeapi.co/api/v1/sprite/'+pokemon+'/',
-    method: 'GET',
-  }).done(function(response) {
-    console.log(response);
-    $("#results").append("<img src=http://pokeapi.co/sprite"+response.image+">");
+  });
   })
 
 });
